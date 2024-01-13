43d2ec446e4adb1b480c9dee9a7235b7f5be86e0
akhileshkrsingh <155524539+akhileshkrsingh@users.noreply.github.com>
2024-01-13 05:26:40 +0000

    modified

--- Dynamic programming/LIS.cpp
+++ Dynamic programming/LIS.cpp
@@ -1,21 +1,25 @@
 #include<iostream>
+#include<string>
 #include<vector>
+#include "../../../usr/include/tcl8.6/tcl-private/compat/string.h"
 using namespace std;
 const int N=1e5+10;
 vector<int>arr(N);
+int dp[N];
 int LIS(int i)
-{
+{if(dp[i]!=-1) return dp[i];
     int ans=1;
    for(int  j=0;j<i;j++){
     if(arr[i]>arr[j]){
         ans=max(ans,LIS(j)+1);
     }
    }
-   return ans;
+   return dp[i]=ans;
 }
 
 int main()
 {
+    memset(dp,-1,sizeof(dp));
     int n;
     cin>>n;
     int arr[n];
